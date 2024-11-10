fcbd97c27661e875d12d2b0980322a71f02b7a95
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-11-10 15:16:31 +0000

    L16 button added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-// todo
+// ---------------------------------------------- NOTES
+
+/*
+[option + shift + f] = format
+*/
+
+// ---------------------------------------------- SET-UP
+
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+// ---------------------------------------------- CONTENT
+
+const button = document.createElement("button");
+button.innerHTML = "click me!";
+app.append(button);
+
+button.addEventListener("click", () => {
+  alert("hello");
+});
