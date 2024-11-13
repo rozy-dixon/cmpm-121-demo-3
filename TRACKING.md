775d70498af9d351e4758b5e7eba91bd0e3fe268
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-11-13 19:50:47 +0000

    d3 step 1 complete

---
+++ TRACKING.md
@@ -0,0 +1,17 @@
+# task tracking
+
+## D3A
+- [x] Present the player with deterministically generated, random-feeling cache locations around the player’s initial location.
+- [x] Deterministically generate an offering of coins to collect at each location.
+- [x] Represent the player’s location and nearby cache locations on the map with details visible either as tooltips or popups.
+- [ ] Allow the player transport coins from one cache to another by collecting and depositing (using buttons in the cache’s popups).
+- [ ] Use a latitude–longitude grid where cells are 0.0001 degrees wide. Initialize the player’s location at the site of our classroom in Oakes College at 36°59'22.2"N 122°03'46.0"W. Place a cache at about 10% of the grid cells that are within 8 cell-steps away from the player’s current location.
+- [ ] Assume that gameplay is permanently anchored around the Oakes College classroom site.
+    - I use player's initial location as the center of gameplay.
+- [ ] Assume that all coins are interchangeable (a collection of the can be sufficiently represented by an integer count).
+
+## D3B
+
+## D3C
+
+## D3D
--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,176 @@
 [option + shift + f] = format
 */
 
-// ---------------------------------------------- SET-UP
+// ---------------------------------------------- IMPORTS
 
+import leaflet from "leaflet";
+
+import "leaflet/dist/leaflet.css";
 import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+import "./leafletWorkaround.ts";
+
+import luck from "./luck.ts";
+import { Marker, Rectangle } from "npm:@types/leaflet@^1.9.14";
+
+// ---------------------------------------------- INITS
+
+const APP_NAME = "orange couscous";
+
+const START_ZOOM = 18;
+const MAX_ZOOM = 19;
+const MIN_ZOOM = 10;
+
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 13;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+// ---------------------------------------------- CLASSES AND INTERFACES
+
+interface Cache {
+  i: number;
+  j: number;
+  pointValue: number;
+  rect: Rectangle;
+}
+
+interface Player {
+  marker: Marker;
+  points: number;
+}
+
+// ---------------------------------------------- SET-UP
+
+document.getElementById("title")!.innerHTML = APP_NAME;
+
+const startPosition = await getPosition();
+
+const map = leaflet.map(document.getElementById("map")!, {
+  center: startPosition,
+  zoom: START_ZOOM,
+  maxZoom: MAX_ZOOM,
+  minZoom: MIN_ZOOM,
+  zoomControl: true,
+  scrollWheelZoom: true,
+});
+
+// create invisible tile layer
+leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: START_ZOOM,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const playerMarker = leaflet.marker(startPosition, {
+  draggable: true,
+  autoPan: true,
+}).addTo(map);
+
+const player: Player = {
+  points: 0,
+  marker: playerMarker,
+};
+
+player.marker.bindTooltip(`${player.points}`);
+
+const cacheArray: Array<Cache> = [];
 
 // ---------------------------------------------- CONTENT
 
-const button = document.createElement("button");
-button.innerHTML = "click me!";
-app.append(button);
+// [ ] on move end, update neighborhood
 
-button.addEventListener("click", () => {
-  alert("hello");
-});
+// [ ] change marker style
+
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      const cache = spawnCache(i, j);
+      cacheArray.push(cache);
+    }
+  }
+}
+
+// ---------------------------------------------- HELPER FUNCTIONS
+
+function spawnCache(i: number, j: number) {
+  const origin: leaflet.LatLng = startPosition;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  const rect = leaflet.rectangle(bounds, {
+    color: "#000000",
+    weight: 2,
+    fillColor: "#ffffff",
+    fillOpacity: 0.5,
+  });
+  rect.addTo(map);
+
+  // src = https://chat.brace.tools/s/8a5b2b11-b475-4e10-8574-fb83e9600379
+  const pointValue: number = Number(
+    Math.floor(luck([i, j, "initialValue"].toString()) * 100),
+  );
+
+  const cache: Cache = {
+    i: i,
+    j: j,
+    pointValue: pointValue,
+    rect: rect,
+  };
+
+  interact(cache);
+
+  return cache;
+}
+
+function interact(cache: Cache) {
+  cache.rect.bindPopup(() => {
+    // src = https://chat.brace.tools/s/136a1f49-5351-482a-a52f-3c15ebcdda58
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+      <div class="pop-up">(${cache.i},${cache.j}), <span id="value">${cache.pointValue}</span>.</div>
+      <button id="collect">collect</button>
+      <button id="deposit">deposit</button>`;
+
+    // collect
+    popupDiv.querySelector<HTMLButtonElement>("#collect")?.addEventListener(
+      "click",
+      () => {
+        cache.pointValue = alterPointValue(Number(cache.pointValue), 1);
+        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = cache
+          .pointValue.toString();
+      },
+    );
+
+    // deposit
+    popupDiv.querySelector<HTMLButtonElement>("#deposit")?.addEventListener(
+      "click",
+      () => {
+        cache.pointValue = alterPointValue(Number(cache.pointValue), -1);
+        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = cache
+          .pointValue.toString();
+      },
+    );
+
+    return popupDiv;
+  });
+}
+
+function getPosition() {
+  // src = https://chat.brace.tools/s/05a34133-2b96-41e8-b9c2-21f0301335d0
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        resolve(
+          leaflet.latLng(position.coords.latitude, position.coords.longitude),
+        );
+      },
+    );
+  });
+}
+
+function alterPointValue(pointValue: number, adjust: number) {
+  player.marker.setTooltipContent(`${player.points += adjust}`);
+  return pointValue -= adjust;
+}
